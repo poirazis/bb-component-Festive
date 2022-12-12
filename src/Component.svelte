e5c6561b4f664469467383cd74937f1223c9c3d8
poirazis <poirazis@gmail.com>
2022-12-12 23:32:05 +0200

    Festive Enough !

--- src/Component.svelte
+++ src/Component.svelte
@@ -11,9 +11,8 @@
 
 <div use:styleable={$component.styles}>
   <ToggleConfetti toggleOnce relative={false}>
-	
-	<div style="position: fixed; top: -50px; left: 0; height: 100vh; width: 100vw; display: flex; justify-content: center; overflow: hidden;">
-		<Confetti x={[-5, 5]} y={[0, 0.1]} delay={[500, 2000]}  infinite duration=5000 amount=400 fallDistance="100vh" />
-	</div>
-</ToggleConfetti>
+    <div style="position: fixed; top: -50px; left: 0; height: 100vh; width: 100vw; display: flex; justify-content: center; overflow: hidden;">
+      <Confetti x={[-5, 5]} y={[0, 0.1]} delay={[500, 2000]}  infinite duration=5000 amount=400 fallDistance="100vh" />
+    </div>
+  </ToggleConfetti>
 </div>
